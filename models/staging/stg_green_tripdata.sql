5646b50b21bf30a0746a5751dce0935f514c0076
Pedro Santiago <ppfsantiago@gmail.com>
2026-02-10 22:57:38 +0000

    adjusting types

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,25 +1,25 @@
 SELECT 
-            --- id
-            CAST(vendorid AS int) as vendor_id,
-            ratecodeid as rate_code_id,
-            pulocationid as pickup_location_id,
-            dolocationid as dropoff_location_id,
-            --- timestamps
-            lpep_pickup_datetime as pickup_datetime,
-            lpep_dropoff_datetime as dropoff_datetime,
-            ---trip info
-            store_and_fwd_flag as store_and_fwd_flag,
-            passenger_count as passenger_count,
-            trip_distance as trip_distance,
-            trip_type as trip_type,
-            --- payment info
-            fare_amount as fare_amount,
-            extra as extra,
-            mta_tax as mta_tax,
-            tip_amount as tip_amount,
-            tolls_amount as tolls_amount,
-            improvement_surcharge as improvement_surcharge,
-            total_amount as total_amount,
-            payment_type as payment_type
+    --- id
+    CAST(vendorid AS int64) as vendor_id,
+    CAST(ratecodeid AS int64) as rate_code_id,
+    CAST(pulocationid AS int64) as pickup_location_id,
+    CAST(dolocationid AS int64) as dropoff_location_id,
+    --- timestamps
+    CAST(lpep_pickup_datetime AS timestamp) as pickup_datetime,
+    CAST(lpep_dropoff_datetime AS timestamp) as dropoff_datetime,
+    ---trip info
+    store_and_fwd_flag as store_and_fwd_flag,
+    CAST(passenger_count AS int64) as passenger_count,
+    CAST(trip_distance AS float64) as trip_distance, 
+    CAST(trip_type AS int64) as trip_type,
+    --- payment info
+    CAST(fare_amount AS numeric) as fare_amount,
+    CAST(extra AS numeric) as extra,
+    CAST(mta_tax AS numeric) as mta_tax,
+    CAST(tip_amount AS numeric) as tip_amount,
+    CAST(tolls_amount AS numeric) as tolls_amount,
+    CAST(improvement_surcharge AS numeric) as improvement_surcharge,
+    CAST(total_amount AS numeric) as total_amount,
+    CAST(payment_type AS int64) as payment_type
 
 FROM {{ source('raw_data', 'green_tripdata') }}
