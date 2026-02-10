d9b2f59cf90f828c241f9acecc380eb39f2ad8ff
Santiago <ppfsantiago@gmail.com>
2026-02-10 20:14:36 -0300

    adding yellow model

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,17 +1,17 @@
 SELECT 
     --- id
-    CAST(vendorid AS int64) as vendor_id,
-    CAST(ratecodeid AS int64) as rate_code_id,
-    CAST(pulocationid AS int64) as pickup_location_id,
-    CAST(dolocationid AS int64) as dropoff_location_id,
+    CAST(vendorid AS integer) as vendor_id,
+    CAST(ratecodeid AS integer) as rate_code_id,
+    CAST(pulocationid AS integer) as pickup_location_id,
+    CAST(dolocationid AS integer) as dropoff_location_id,
     --- timestamps
     CAST(lpep_pickup_datetime AS timestamp) as pickup_datetime,
     CAST(lpep_dropoff_datetime AS timestamp) as dropoff_datetime,
     ---trip info
     store_and_fwd_flag as store_and_fwd_flag,
-    CAST(passenger_count AS int64) as passenger_count,
-    CAST(trip_distance AS float64) as trip_distance, 
-    CAST(trip_type AS int64) as trip_type,
+    CAST(passenger_count AS integer) as passenger_count,
+    CAST(trip_distance AS numeric) as trip_distance, 
+    CAST(trip_type AS integer) as trip_type,
     --- payment info
     CAST(fare_amount AS numeric) as fare_amount,
     CAST(extra AS numeric) as extra,
@@ -20,6 +20,7 @@ SELECT
     CAST(tolls_amount AS numeric) as tolls_amount,
     CAST(improvement_surcharge AS numeric) as improvement_surcharge,
     CAST(total_amount AS numeric) as total_amount,
-    CAST(payment_type AS int64) as payment_type
+    CAST(payment_type AS integer) as payment_type
 
 FROM {{ source('raw_data', 'green_tripdata') }}
+WHERE vendorid IS NOT NULL
