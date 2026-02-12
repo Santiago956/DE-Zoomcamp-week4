9569c5f513135f5d00a206f0b3d05e54b469aea3
Pedro Santiago <ppfsantiago@gmail.com>
2026-02-12 00:14:55 +0000

    Dim update

--- models/marts/dim_vendors.sql
+++ models/marts/dim_vendors.sql
@@ -1,1 +1,12 @@
-SELECT 1
+WITH trips_unioned AS (
+    SELECT * FROM {{ ref('int_trips_unioned') }}
+),
+
+vendors AS (
+    SELECT
+        DISTINCT vendor_id,
+        {{ get_vendor_names('vendor_id')}} AS vendor_names
+    FROM trips_unioned
+)
+
+SELECT * FROM vendors
--- models/marts/fact_trips.sql
+++ models/marts/fact_trips.sql
@@ -1,1 +1,10 @@
-SELECT 1
+WITH trips_unioned AS (
+    SELECT * FROM {{ref('int_trips_unioned')}}
+)
+
+SELECT
+    GENERATE_UUID() AS trip_id,
+    COUNT(*) AS duplicates
+FROM trips_unioned
+GROUP BY 1
+HAVING COUNT(*) > 1
